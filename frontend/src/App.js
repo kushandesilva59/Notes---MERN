9448b30ea7afcda3351c22baa16c45b31f483af6
kushandesilva59 <kushandesilva801@gmail.com>
2024-02-12 21:33:51 +0530

    create routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,21 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import LangingPage from "./screens/LandingPage/LangingPage";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import MyNotes from "./components/MyNotes/MyNotes";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main>
-        <LangingPage />
+        <Routes>
+          <Route path="/" element={<LangingPage />} />
+          <Route path="/mynotes" element={<MyNotes />} />
+        </Routes>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,18 +1,15 @@
 import React from "react";
-import {
-  Nav,
-  Navbar,
-  NavDropdown,
-  Container,
-  Form,
-} from "react-bootstrap";
+import { Nav, Navbar, NavDropdown, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
       <Navbar expand="lg" className="bg-primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <Link to="/"> Navbar scroll</Link>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="m-auto">
@@ -27,15 +24,15 @@ const Header = () => {
             </Nav>
 
             <Nav navbarScroll>
-              <Nav.Link href="#action1">My Notes</Nav.Link>
+              <Nav.Link href="/mynotes">
+                <Link to="/mynotes">My Notes</Link>
+              </Nav.Link>
 
               <NavDropdown title="Kushan De Silva" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">My Profile</NavDropdown.Item>
-                
+
                 <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Log Out
-                </NavDropdown.Item>
+                <NavDropdown.Item href="#action5">Log Out</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
