dc6b76c1277aee9ef17e8f1fcd6164a2a832a2cc
kushandesilva59 <kushandesilva801@gmail.com>
2024-02-12 20:29:47 +0530

    init frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main style={{minHeight: '93vh'}}></main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header/Header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+  Container,
+  Form,
+} from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="m-auto">
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+              </Form>
+            </Nav>
+
+            <Nav navbarScroll>
+              <Nav.Link href="#action1">My Notes</Nav.Link>
+
+              <NavDropdown title="Kushan De Silva" id="navbarScrollingDropdown">
+                <NavDropdown.Item href="#action3">My Profile</NavDropdown.Item>
+                
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action5">
+                  Log Out
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
