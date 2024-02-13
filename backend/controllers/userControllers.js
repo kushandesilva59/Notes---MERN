256e90a18a9e9ceca0ce3024af3a6701d9e2d9eb
kushandesilva59 <kushandesilva801@gmail.com>
2024-02-13 21:45:35 +0530

    successfully insert user to Db using postman

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,11 +1,38 @@
-const registerUser = async (req, res) => {
-    const { name, email, password, pic} = req.body;
-    console.log(name,email)
+const User = require("../models/userModel");
+const asynchandler = require("express-async-handler");
+const { use } = require("../routes/userRoute");
 
-    res.json({
-        name,
-        email
+const registerUser = asynchandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+  console.log(name, email);
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists!...");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if(user){
+    res.status(201).json({
+        _id:user._id,
+        name:user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        pic: user.pic
     })
-}
+  }else{
+    res.status(400)
+    throw new Error('Error Occured!..')
+  }
 
-module.exports={registerUser}
+});
+
+module.exports = { registerUser };
