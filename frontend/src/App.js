7e448e53705cf3c700c818a2953087a26f5220df
kushandesilva59 <kushandesilva801@gmail.com>
2024-02-13 13:30:02 +0530

    add accordion to mynotes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/footer/Footer";
 import LangingPage from "./screens/LandingPage/LangingPage";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import MyNotes from "./components/MyNotes/MyNotes";
+import Test from "./components/Test/Test";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LangingPage />} />
           <Route path="/mynotes" element={<MyNotes />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </main>
       <Footer />
--- frontend/src/components/MyNotes/MyNotes.js
+++ frontend/src/components/MyNotes/MyNotes.js
@@ -1,22 +1,18 @@
 import React, { useEffect } from "react";
 import MainScreen from "../MainScreen";
-import { Button, Card } from "react-bootstrap";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import  notes  from "../../data/notes.js";
+import notes from "../../data/notes.js";
 
 const MyNotes = () => {
-
-
-  useEffect(()=>{
-    console.log(notes)
-  })
+  useEffect(() => {
+    console.log(notes);
+  });
 
   const deleteHandler = (id) => {
-    if(window.confirm("Are you sure?")){
-
+    if (window.confirm("Are you sure?")) {
     }
-
-  }
+  };
 
   return (
     <MainScreen title="Welcome back kushan de sivla">
@@ -25,46 +21,51 @@ const MyNotes = () => {
           Create New Note
         </Button>
       </Link>
-      {notes.map((note) => (
-        <Card style={{ margin: 10 }}>
-          <Card.Header style={{ display: "flex" }}>
-            <span
-              style={{
-                color: "black",
-                textDecoration: "none",
-                flex: 1,
-                cursor: "pointer",
-                alignSelf: "center",
-                fontSize: 18,
-              }}
-            >
-              {note.title}
-            </span>
-
-            <div>
-              <Button href={`/note/${note._id}`}>Edit</Button>
-              <Button
-                variant="danger"
-                className="mx-2"
-                onClick={() => deleteHandler(note._id)}
+      <Accordion>
+        {notes.map((note) => (
+          <Card key={note._id} style={{ margin: 10 }}>
+            <Card.Header style={{ display: "flex" }}>
+              <span
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                  flex: 1,
+                  cursor: "pointer",
+                  alignSelf: "center",
+                  fontSize: 18,
+                }}
               >
-                Delete
-              </Button>
-            </div>
-          </Card.Header>
+                <Accordion.Header  variant="link" eventKey="0">
+                  {note.title}
+                </Accordion.Header>
+              </span>
 
-          <Card.Body>
-            <blockquote className="blockquote mb-0">
-              <p>
-               {note.content}
-              </p>
-              <footer className="blockquote-footer">
-                  Create on -date
-              </footer>
-            </blockquote>
-          </Card.Body>
-        </Card>
-      ))}
+              <div>
+                <Button href={`/note/${note._id}`}>Edit</Button>
+                <Button
+                  variant="danger"
+                  className="mx-2"
+                  onClick={() => deleteHandler(note._id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </Card.Header>
+
+            <Accordion.Body eventKey="">
+              <Card.Body>
+                <h4>
+                  <Badge variant="success">Category - {note.category}</Badge>
+                </h4>
+                <blockquote className="blockquote mb-0">
+                  <p>{note.content}</p>
+                  <footer className="blockquote-footer">Create on -date</footer>
+                </blockquote>
+              </Card.Body>
+            </Accordion.Body>
+          </Card>
+        ))}
+      </Accordion>
     </MainScreen>
   );
 };
